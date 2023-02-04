02d7b8363bbab653a80a382e43e8a8ac24802ab3
oriane corral <shining.moonsunshine@gmail.com>
2023-02-04 16:53:38 +0100

    activities new view

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,6 @@
 class Activity < ApplicationRecord
-  belongs_to :itinerary dependent: :destroy
+  belongs_to :itinerary, dependent: :destroy
   belongs_to :category
+  belongs_to :user
   validates :title, :address, :description, presence: true
 end
