c48b58ebeffe72014343a83ba17b651d97927d90
oriane corral <shining.moonsunshine@gmail.com>
2023-02-07 21:49:36 +0100

    edit view & routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :itineraries do
-    resources :activities
+    resources :activities, only: [:new, :create, :index]
     resources :reviews
   end
+  resources :activities, except: [:new, :create, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
