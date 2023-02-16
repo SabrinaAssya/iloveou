4116be92fa8e184c78c2765038867168e4392916
Emma Metral <emma.mtr@gmail.com>
2023-02-16 19:41:46 +0100

    front de review

--- db/schema.rb
+++ db/schema.rb
@@ -86,7 +86,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_11_135113) do
   create_table "reviews", force: :cascade do |t|
     t.bigint "itinerary_id", null: false
     t.bigint "user_id", null: false
-    t.integer "rating"
+    t.float "rating"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
