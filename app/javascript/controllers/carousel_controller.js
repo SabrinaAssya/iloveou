e5e630622224696cbeaf9972282e8a603ad1fcc9
Sabrina Achab <sabrina.achab@hotmail.com>
2023-02-23 14:52:56 +0100

    stimulus carousel need help

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -7,10 +7,26 @@ export default class extends Controller {
     console.log("hello from carousel controller stimulus");
     console.log(this.element);
     console.log(this.carouselTarget);
+    console.log(this.cardTarget);
+    console.log(this.element.clientWidth)
+
+    const arrowIcons = this.element.querySelectorAll("i")
+    const firstCard = this.carouselTarget.querySelectorAll("card-slider")[0];
+    let firstCardWidth = firstCard.this.element.clientWidth + 50;
+    arrowIcons.forEach(icon => {
+      icon.addEventListener("click", () => {
+        this.carouselTarget.scrollLeft += icon.id == "left" ? -firstCardWidth : firstCardWidth;
+      })
+    });
   }
 
-  scroll() {
-    console.log("coucou2")
-    console.log(this.element.event)
-  }
+  /* scroll() {
+    const arrowIcons = this.element.querySelectorAll("i")
+    arrowIcons.forEach(icon => {
+      icon.addEventListener("click", () => {
+        console.log(icon)
+      })
+    });
+
+  } */
 }
