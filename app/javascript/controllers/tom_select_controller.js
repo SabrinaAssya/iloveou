f3f054951a55e491bc32376b12c733db65599dd6
Sabrina Achab <sabrina.achab@hotmail.com>
2023-02-28 22:11:37 +0100

    search for homepage done with some fixing of carousel

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -2,8 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 import TomSelect from "tom-select";
 
 export default class extends Controller {
+  static values = {
+    checkbox: Boolean
+  }
   connect() {
     console.log("coucou")
-    new TomSelect(this.element)
+    console.log(this.element.attributes[3])
+    if (this.checkboxValue === true) {
+      this.tom = new TomSelect(this.element, {
+        plugins: ['input_autogrow'],
+      })
+    } else {
+      this.tom = new TomSelect(this.element)
+    }
   }
+
+  display() {
+    this.tom.settings.placeholder = "";
+    this.tom.inputState();
+  }
+
 }
